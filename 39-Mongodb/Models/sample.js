9742c3cf8716c8d42ee60a8bd7faa81b6ef53d34
soyachandracs <soya.chandra@duk.ac.in>
2025-02-05 10:02:46 +0530

    update

---
+++ 39-Mongodb/Models/sample.js
@@ -0,0 +1,14 @@
+//import mongoose from 'mongoose';
+
+//const { Schema, model } = mongoose; 
+import {Schema} from 'mongoose';
+import {model} from 'mongoose';
+
+const demo=new Schema({
+    userid:{type:String,required:true},
+    name:{type:String,required:true},
+    dob:{type:String,required:true}
+});
+const sample=model('sample1',demo)
+
+export default sample;
