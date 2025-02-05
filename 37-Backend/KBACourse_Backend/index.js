57c39a9e5e8652ed274f8363c953f7712207b8d7
pglekshmi <pglekshmi88@gmail.com>
2025-02-05 09:53:25 +0530

    🔥| Backend

---
+++ 37-Backend/KBACourse_Backend/index.js
@@ -0,0 +1,38 @@
+import express,{json} from 'express';
+import dotenv from 'dotenv';
+import { userauth } from './Routes/userroute.js';
+import adminauth from './Routes/adminroute.js';
+import adminsign from './Routes/adminsign.js';
+import cors from 'cors';
+
+
+dotenv.config();
+ 
+const app=express();
+
+app.use(cors({
+    origin:'*',
+    credentials:true
+}))
+
+app.use(json())
+
+app.use('/',userauth);
+app.use('/',adminauth);
+app.use('/admin',adminsign);
+
+
+app.get('/',function(req,res){
+    // console.log("HI");
+    res.send("Hello Everyone");
+})
+
+app.post('/',function(req,res){
+    res.send("Hello Everyone");
+})
+
+
+
+app.listen(process.env.PORT,function(){
+    console.log(`server is listening at ${process.env.PORT}`);
+});
