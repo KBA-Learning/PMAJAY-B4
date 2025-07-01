47480dfe25f62336a2f083d69302d017af969cbc
soyachandracs <soya.chandra@duk.ac.in>
2025-07-01 11:03:30 +0530

    update

--- 8-CHF/Client/client.go
+++ 8-CHF/Client/client.go
@@ -1,119 +1,89 @@
 package main
 
 import (
+	"fmt"
+	"time"
 
-"fmt"
-
-"github.com/hyperledger/fabric-gateway/pkg/client"
-
+	"github.com/hyperledger/fabric-gateway/pkg/client"
 )
 
 // Submit a transaction synchronously, blocking until it has been committed to the ledger.
-
 func submitTxnFn(organization string, channelName string, chaincodeName string, contractName string, txnType string, privateData map[string][]byte, txnName string, args ...string) string {
 
-orgProfile := profile[organization]
+	orgProfile := profile[organization]
+	mspID := orgProfile.MSPID
+	certPath := orgProfile.CertPath
+	keyPath := orgProfile.KeyDirectory
+	tlsCertPath := orgProfile.TLSCertPath
+	gatewayPeer := orgProfile.GatewayPeer
+	peerEndpoint := orgProfile.PeerEndpoint
 
-mspID := orgProfile.MSPID
+	// The gRPC client connection should be shared by all Gateway connections to this endpoint
+	clientConnection := newGrpcConnection(tlsCertPath, gatewayPeer, peerEndpoint)
+	defer clientConnection.Close()
 
-certPath := orgProfile.CertPath
+	id := newIdentity(certPath, mspID)
+	sign := newSign(keyPath)
 
-keyPath := orgProfile.KeyDirectory
+	// Create a Gateway connection for a specific client identity
+	gw, err := client.Connect(
+		id,
+		client.WithSign(sign),
+		client.WithClientConnection(clientConnection),
+		// Default timeouts for different gRPC calls
+		client.WithEvaluateTimeout(5*time.Second),
+		client.WithEndorseTimeout(15*time.Second),
+		client.WithSubmitTimeout(5*time.Second),
+		client.WithCommitStatusTimeout(1*time.Minute),
+	)
+	if err != nil {
+		panic(err)
+	}
+	defer gw.Close()
 
-tlsCertPath := orgProfile.TLSCertPath
+	network := gw.GetNetwork(channelName)
+	contract := network.GetContractWithName(chaincodeName, contractName)
+	fmt.Printf("\n-->Submiting Transaction: %s,\n", txnName)
 
-gatewayPeer := orgProfile.GatewayPeer
+	switch txnType {
+	case "invoke":
+		result, err := contract.SubmitTransaction(txnName, args...)
 
-peerEndpoint := orgProfile.PeerEndpoint
+		if err != nil {
+			panic(fmt.Errorf("failed to submit transaction: %w", err))
+		}
+		return fmt.Sprintf("*** Transaction submitted successfully: %s\n", result)
 
-// The gRPC client connection should be shared by all Gateway connections to this endpoint
+	case "query":
+		evaluateResult, err := contract.EvaluateTransaction(txnName, args...)
+		if err != nil {
+			panic(fmt.Errorf("failed to evaluate transaction: %w", err))
+		}
 
-clientConnection := newGrpcConnection(tlsCertPath, gatewayPeer, peerEndpoint)
+		// return fmt.Sprintf("*** Result:%s\n", result)
+		var result string
+		if isByteSliceEmpty(evaluateResult) {
+			result = string(evaluateResult)
+		} else {
+			result = formatJSON(evaluateResult)
+		}
 
-defer clientConnection.Close()
+		// return fmt.Sprintf("*** Result:%s\n", result)
+		return result
 
-id := newIdentity(certPath, mspID)
+	case "private":
+		result, err := contract.Submit(
+			txnName,
+			client.WithArguments(args...),
+			client.WithTransient(privateData),
+		)
 
-sign := newSign(keyPath)
+		if err != nil {
+			panic(fmt.Errorf("failed to submit transaction: %w", err))
+		}
 
-// Create a Gateway connection for a specific client identity
+		return fmt.Sprintf("*** Transaction committed successfully\n result: %s \n", result)
 
-gw, err := client.Connect(
-
-id,
-
-client.WithSign(sign),
-
-client.WithClientConnection(clientConnection),
-
-)
-
-if err != nil {
-
-panic(err)
-
-}
-
-defer gw.Close()
-
-network := gw.GetNetwork(channelName)
-
-contract := network.GetContractWithName(chaincodeName, contractName)
-
-fmt.Printf("\n-->Submiting Transaction: %s,\n", txnName)
-
-switch txnType {
-
-case "invoke":
-
-result, err := contract.SubmitTransaction(txnName, args...)
-
-if err != nil {
-
-panic(fmt.Errorf("failed to submit transaction: %w", err))
-
-}
-
-return fmt.Sprintf("*** Transaction submitted successfully: %s\n", result)
-
-case "query":
-
-evaluateResult, err := contract.EvaluateTransaction(txnName, args...)
-
-if err != nil {
-
-panic(fmt.Errorf("failed to evaluate transaction: %w", err))
-
-}
-
-result := formatJSON(evaluateResult)
-
-return fmt.Sprintf("*** Result:%s \n", result)
-
-case "private":
-
-result, err := contract.Submit(
-
-txnName,
-
-client.WithArguments(args...),
-
-client.WithTransient(privateData),
-
-)
-
-if err != nil {
-
-panic(fmt.Errorf("failed to submit transaction: %w", err))
-
-}
-
-fmt.Println(result)
-
-return fmt.Sprintf("*** Transaction committed successfullyn result: %s \n", result)
-
-}
-
-return ""
-
+	}
+	return ""
 }
--- 8-CHF/Client/main.go
+++ 8-CHF/Client/main.go
@@ -1,65 +1,41 @@
 package main
 
+import "fmt"
+
 func main() {
 
-// use these functions to evaluate and submit transactions
+	// use this functions to evaluate and submit txns
+	// try calling these functions
 
-// try calling these functions
+	// result := submitTxnFn(
+	// 	"org1",
+	// 	"autochannel",
+	// 	"KBA-Automobile",
+	// 	"CarContract",
+	// 	"invoke",
+	// 	make(map[string][]byte),
+	// 	"CreateCar",
+	// 	"Car-06",
+	// 	"Maruti",
+	// 	"Alto",
+	// 	"Red",
+	// 	"fac01",
+	// 	"25/10/2023",
+	// )
 
-submitTxnFn(
+	// privateData := map[string][]byte{
+	// 	"make":       []byte("Maruti"),
+	// 	"model":      []byte("Alto"),
+	// 	"color":      []byte("Red"),
+	// 	"dealerName": []byte("Popular"),
+	// }
 
-"org1",
-
-"autochannel",
-
-"KBA-Automobile",
-
-"CarContract",
-
-"invoke",
-
-make(map[string][]byte),
-
-"CreateCar",
-
-"Car-08",
-
-"Tata",
-
-"Harrier",
-
-"Black",
-
-"fac01",
-
-"25/10/2023",
-
-)
-
-// privateData := map[string][]byte{
-
-// "make": []byte("Maruti"),
-
-// "model": []byte("Alto"),
-
-// "color": []byte("Red"),
-
-// "dealerName": []byte("Popular"),
-
-// }
-
-// submitTxnFn("org2", "autochannel", "KBA-Automobile", "OrderContract", "private", make(map[string][]byte), "CreateOrder", "ORD-03")
-
-// submitTxnFn("org2", "autochannel", "KBA-Automobile", "OrderContract", "query", make(map[string][]byte), "ReadOrder", "ORD-05")
-
-//submitTxnFn("org1", "autochannel", "KBA-Automobile", "CarContract", "query", make(map[string][]byte), "GetAllCars")
-
-// submitTxnFn("org1", "autochannel", "KBA-Automobile", "OrderContract", "query", make(map[string][]byte),"GetAllOrders")
-
-// submitTxnFn("org1", "autochannel", "KBA-Automobile", "CarContract", "query", make(map[string][]byte),"GetMatchingOrders", "Car-06")
-
-// submitTxnFn("org1", "autochannel", "KBA-Automobile", "CarContract", "invoke", make(map[string][]byte),"MatchOrder", "Car-06", "ORD-01")
-
-// submitTxnFn("org3", "autochannel", "KBA-Automobile", "CarContract", "invoke", make(map[string][]byte),"RegisterCar", "Car-06", "Dani", "KL-01-CD-01")
-
+	 //result := submitTxnFn("org2", "autochannel", "KBA-Automobile", "OrderContract", "private", privateData, "CreateOrder", "ORD-05")
+	 result := submitTxnFn("org2", "autochannel", "KBA-Automobile", "OrderContract", "query", make(map[string][]byte), "ReadOrder", "ORD-03")
+	 //result := submitTxnFn("org1", "autochannel", "KBA-Automobile", "CarContract", "query", make(map[string][]byte), "GetAllCars")
+	// result := submitTxnFn("org1", "autochannel", "KBA-Automobile", "OrderContract", "query", make(map[string][]byte), "GetAllOrders")
+	// result := submitTxnFn("org1", "autochannel", "KBA-Automobile", "CarContract", "query", make(map[string][]byte), "GetMatchingOrders", "Car-06")
+	// result := submitTxnFn("org1", "autochannel", "KBA-Automobile", "CarContract", "invoke", make(map[string][]byte), "MatchOrder", "Car-06", "ORD-03")
+	// result := submitTxnFn("org3", "autochannel", "KBA-Automobile", "CarContract", "invoke", make(map[string][]byte), "RegisterCar", "Car-06", "Dani", "KL-01-CD-01")
+	fmt.Println(result)
 }
